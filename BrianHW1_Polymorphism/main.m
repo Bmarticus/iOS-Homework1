51fc6a5e25d3da1237634c5ef1e78d2ffe84eda2
Brian Martinez <brian@Brians-MacBook-Air.local>
2017-09-26 21:32:34 -0400

    Initial Commit

---
+++ BrianHW1_Polymorphism/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  BrianHW1_Polymorphism
+//
+//  Created by Brian Martinez on 9/26/17.
+//  Copyright © 2017 Brian Martinez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
